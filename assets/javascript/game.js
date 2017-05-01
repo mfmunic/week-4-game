53bfdcde4d88f2dc2267b66597074f6884a0049f
mfmunic <mfmunic@gmail.com>
2017-05-01 14:50:30 -0700

    Finished

---
+++ assets/javascript/game.js
@@ -0,0 +1,73 @@
+var win = 0;
+var loss = 0;
+
+function jewelGame() {
+
+	var guessTotal = 0;
+
+	//declare 4 variables that will never equal each other
+	var jewel1 = Math.ceil(Math.random()*3);
+	var jewel2 = Math.ceil(Math.random()*3)+3;
+	var jewel3 = Math.ceil(Math.random()*3)+6;
+	var jewel4 = Math.ceil(Math.random()*3)+9;
+
+	//make a random number that with a min equal to the sum of the above var
+	var jewels = jewel1+jewel2+jewel3+jewel4;
+	var goal = Math.floor(Math.random()*(100-jewels))+jewels;
+
+	console.log(jewel1, jewel2, jewel3, jewel4);
+
+	$("#crystalNo").text(goal);
+	$("#wins").text(win);
+	$("#losses").text(loss);
+	$("#yourTotal").text(guessTotal);
+
+	$("#jewel1").on("click",function(){
+		guessTotal += jewel1;
+		$("#yourTotal").text(guessTotal);
+		if (guessTotal > goal){
+			loss++;
+			jewelGame();
+		} else if (guessTotal === goal){
+			win++;
+			jewelGame();
+			}
+	})//end of onclick 1
+
+	$("#jewel2").on("click",function(){
+		guessTotal += jewel2;
+		$("#yourTotal").text(guessTotal);
+		if (guessTotal > goal){
+			loss++;
+			jewelGame();
+		} else if (guessTotal === goal){
+			win++;
+			jewelGame();
+			}
+	})//end of onclick 2
+
+	$("#jewel3").on("click",function(){
+		guessTotal += jewel3;
+		$("#yourTotal").text(guessTotal);
+		if (guessTotal > goal){
+			loss++;
+			jewelGame();
+		} else if (guessTotal === goal){
+			win++;
+			jewelGame();
+			}
+	})//end of onclick 3
+
+	$("#jewel4").on("click",function(){
+		guessTotal += jewel4;
+		$("#yourTotal").text(guessTotal);
+		if (guessTotal > goal){
+			loss++;
+			jewelGame();
+		} else if (guessTotal === goal){
+			win++;
+			jewelGame();
+			}
+	})//end of onclick 4
+
+}//end of function
