2aac0d4f33a99254f49e6f6be58df814f673bc5f
mfmunic <mfmunic@gmail.com>
2017-05-05 12:21:09 -0700

    final product

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,11 @@ var loss = 0;
 var guessTotal = 0;
 var goal = 0;
 
+var jewel1;
+var jewel2;
+var jewel3;
+var jewel4;
+
 jewelGame()
 
 function jewelGame() {
@@ -19,61 +24,57 @@ function jewelGame() {
 	$("#crystalNo").text(goal);
 
 	//declare 4 variables that will never equal each other
-	var jewel1 = Math.ceil(Math.random()*12);
+	jewel1 = Math.ceil(Math.random()*12);
 
 	do {
-		var jewel2 = Math.ceil(Math.random()*12);
+		jewel2 = Math.ceil(Math.random()*12);
 	} while (jewel2 == jewel1);
 
 	do {
-		var jewel3 = Math.ceil(Math.random()*12);
+		jewel3 = Math.ceil(Math.random()*12);
 	} while (jewel3 == jewel1 || jewel3 == jewel2);
 
 	do {
-		var jewel4 = Math.ceil(Math.random()*12);
+		jewel4 = Math.ceil(Math.random()*12);
 	} while (jewel4 == jewel1 || jewel4 == jewel2 || jewel4 == jewel3);
 
 	console.log(jewel1, jewel2, jewel3, jewel4);
-
-	$("#jewel1").on("click",function(){
-		guessTotal += jewel1;
-		console.log("jewel1 is "+jewel1);
-		$("#yourTotal").text(guessTotal);
-		check();
-	})//end of onclick 1
-
-	$("#jewel2").on("click",function(){
-		guessTotal += jewel2;
-		console.log("jewel2 is "+jewel2);
-		$("#yourTotal").text(guessTotal);
-		check();
-	})//end of onclick 2
-
-	$("#jewel3").on("click",function(){
-		guessTotal += jewel3;
-		console.log("jewel3 is "+jewel3);
-		$("#yourTotal").text(guessTotal);
-		check();
-	})//end of onclick 3
-
-	$("#jewel4").on("click",function(){
-		guessTotal += jewel4;
-		console.log("jewel4 is "+jewel4);
-		$("#yourTotal").text(guessTotal);
-		check();
-	})//end of onclick 4
-
 }//end of function
+
+$("#jewel1").on("click",function(){
+	guessTotal += jewel1;
+	$("#yourTotal").text(guessTotal);
+	showValue("#jewel1", jewel1);
+	check();
+})//end of onclick 1
+
+$("#jewel2").on("click",function(){
+	guessTotal += jewel2;
+	$("#yourTotal").text(guessTotal);
+	showValue();
+	check();
+})//end of onclick 2
+
+$("#jewel3").on("click",function(){
+	guessTotal += jewel3;
+	$("#yourTotal").text(guessTotal);
+	showValue();
+	check();
+})//end of onclick 3
+
+$("#jewel4").on("click",function(){
+	guessTotal += jewel4;
+	$("#yourTotal").text(guessTotal);
+	showValue();
+	check();
+})//end of onclick 4
 
 //checks win lose conditions
 function check(){
-	console.log("guesstotal is "+guessTotal);
 	if (guessTotal > goal){
-		console.log("loss");
 		loss++;
 		jewelGame();
-		} else if (guessTotal == goal){
-		console.log("win");
+	} else if (guessTotal == goal){
 		win++;
 		jewelGame();
 	}
